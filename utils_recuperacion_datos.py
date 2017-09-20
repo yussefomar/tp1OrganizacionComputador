2ccc04104b2c60e539f6df979ee41e0777bc9a5e
pintotomas <tomasp834@gmail.com>
2017-09-20 03:08:10 -0300

    Recupero place_name con geopy (todavia no termino de correr)

--- utils_recuperacion_datos.py
+++ utils_recuperacion_datos.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 def recuperar_superficie(rowProperati):
     if pd.isnull(rowProperati['surface_total_in_m2']):
@@ -39,4 +40,79 @@ def recuperar_ppm2(rowProperati):
             return rowProperati['price_usd_per_m2']
     else:
         return rowProperati['price_usd_per_m2']
+
+
+#Hay bastantes registros de capital federal que tienen como 'place_name' a 'Capital Federal'
+#Se intenta recuperar la verdadera localidad usando geopy.
+from geopy.geocoders import Nominatim
+barrios_capital = [u' Agronomía',
+    u' Almagro',
+    u' Balvanera',
+    u' Barracas',
+    u' Belgrano',
+    u' Boedo',
+    u' Caballito',
+    u' Chacarita',
+    u' Coghlan',
+    u' Colegiales',
+    u' Constitución',
+    u' Flores',
+    u' Floresta',
+    u' La Boca',
+    u' La Paternal',
+    u' Liniers',
+    u' Mataderos',
+    u' Monte Castro',
+    u' Monserrat',
+    u' Nueva Pompeya',
+    u' Núñez',
+    u' Palermo',
+    u' Parque Avellaneda',
+    u' Parque Chacabuco',
+    u' Parque Chas',
+    u' Parque Patricios',
+    u' Puerto Madero',
+    u' Recoleta',
+    u' Retiro',
+    u' Saavedra',
+    u' San Cristóbal',
+    u' San Nicolás',
+    u' San Telmo',
+    u' Vélez Sársfield',
+    u' Versalles',
+    u' Villa Crespo',
+    u' Villa del Parque',
+    u' Villa Devoto',
+    u' Villa General Mitre',
+    u' Villa Lugano',
+    u' Villa Luro',
+    u' Villa Ortúzar',
+    u' Villa Pueyrredón',
+    u' Villa Real',
+    u' Villa Riachuelo',
+    u' Villa Santa Rita',
+    u' Villa Soldati',
+    u' Villa Urquiza',
+ ]
+geolocator = Nominatim()
+
+def recuperar_barrio_capital(row):
+	"""PRE: row es un registro de capital federal con barrio incorrecto
+	POST: devuelve el place_name corregido o el original si no hacia falta corregir.
+	Si no se puede corregir porque no se tiene la lat-lon, devuelve 'Capital Federal'"""
+	
+	if not(pd.isnull(row["lat-lon"])):
+		
+		info = geolocator.reverse(row["lat-lon"])
+		a = info.address
+		if (a != None):
+			direccion = a.split(",")
+		else:
+			return row["place_name"]
+			
+		for b in barrios_capital:
+			if b in direccion:
+				
+				return b
+	return row["place_name"]
 
