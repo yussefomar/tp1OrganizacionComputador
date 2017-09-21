1fb29eabbd6bdf8899d0cbd9014f2b9f9727c361
pintotomas <tomasp834@gmail.com>
2017-09-21 20:01:12 -0300

    Posiciones de subtes y trenes en CABA para analisis

---
+++ pos_subtes_y_trenes_capital.py
@@ -0,0 +1,142 @@
+
+#lineaA
+sanPedrito = (-34.631020, -58.470691)
+sanJoseFlores = (-34.629216, -58.463720)
+carabobo = (-34.626574, -58.456292)
+puan = (-34.623601, -58.448918)
+primeraJunta = (-34.623601, -58.448918)
+acoyte = (-34.618109, -58.435999)
+rioJaneiro = (-34.615164, -58.429405)
+castroBarros = (-34.611780, -58.421831)
+loria = (-34.610760, -58.415312)
+plazaMiserere = (-34.610072, -58.406855)
+alberti = (-34.609934, -58.400881)
+pasco = (-34.609598, -58.398220)
+congreso = (-34.609239, -58.392481)
+saenz = (-34.609427, -58.386700)
+lima = (-34.609152, -58.382512)
+piedras = (-34.608876, -58.378667)
+peru = (-34.608566, -58.374611)
+plazaDeMayo = (-34.608749, -58.370640)
+
+lineaA =[sanPedrito,sanJoseFlores,carabobo,puan,primeraJunta,\
+ acoyte,rioJaneiro,castroBarros,loria,plazaMiserere, alberti,\
+ pasco, congreso, saenz, lima, piedras, peru, plazaDeMayo]
+
+#linea b
+alem = (-34.602934, -58.369500)
+florida = (-34.603323, -58.374565)
+uruguay = (-34.604156, -58.386880)
+callao_b = (-34.604572, -58.392942)
+pasteur =(-34.604615, -58.399228)
+pueyrredon = (-34.604618, -58.405106)
+carlosGardel = (-34.604130, -58.411589)
+medrano = (-34.603186, -58.420984)
+Gallardo = (-34.602140, -58.430910)
+malabia = (-34.599135, -58.439580)
+dorrego = (-34.591748, -58.447571)
+lacroze = (-34.586862, -58.455399)
+tronador = (-34.584158, -58.466606)
+los_incas = (-34.581660, -58.473889)
+echeverria = (-34.578096, -58.480809)
+jmrosas = (-34.573965, -58.487630)
+
+lineaB = [alem, florida, uruguay, callao_b, pasteur,\
+ pueyrredon, carlosGardel, medrano, Gallardo, malabia, dorrego, lacroze, tronador,\
+ los_incas,echeverria,jmrosas]
+
+#linea c
+constitucion = (-34.628037, -58.380957)
+sjuan = (-34.622383, -58.379843)
+independencia = (-34.618165, -58.380341)
+marianoMoreno = (-34.612435, -58.380504)
+avDeMayo = (-34.609016, -58.380772)
+diagonalNorte = (-34.604950, -58.379253)
+lavalle = (-34.602057, -58.378141)
+sanMartin = (-34.595543, -58.377464)
+retiro = (-34.590549, -58.376075)
+
+lineaC = [constitucion, sjuan, independencia, marianoMoreno, avDeMayo,\
+	diagonalNorte, lavalle, sanMartin, retiro]
+
+#linea d
+congreso = (-34.556147, -58.461971) #Linea D
+juramento = (-34.562541, -58.456528) #Linea D
+joseHernandez = (-34.566129, -58.452241)
+olleros = (-34.556147, -58.461971)
+ministroCarranza = (-34.575375, -58.434435)
+palermo = (-34.578543, -58.426370)
+plazaItalia = (-34.581279, -58.421510)
+scalabrini = (-34.585220, -58.415856)
+bulnes = (-34.588338, -58.411254)
+aguero = (-34.591597, -58.407195)
+pueyrredon = (-34.594641, -58.402524)
+medicina = (-34.599564, -58.397987)
+callao = (-34.599495, -58.392838)
+tribunales = (-34.601886, -58.385331)
+pellegrini = (-34.603747, -58.381368)
+nueveDeJulio = (-34.604411, -58.380265)
+catedral = (-34.607651, -58.374375)
+
+lineaD = [congreso, juramento, joseHernandez, olleros, ministroCarranza, palermo, plazaItalia,\
+	scalabrini, bulnes,aguero,pueyrredon,medicina,callao,tribunales,pellegrini,nueveDeJulio,\
+	catedral]
+
+#lineaE
+virreyes = (-34.642909, -58.461442)
+varela = (-34.639537, -58.457826)
+mmilagrosa = (-34.636424, -58.450063)
+emitre = (-34.631651, -58.442505)
+jmmoreno = (-34.628093, -58.434307)
+laplata = (-34.626813, -58.426143)
+boedo = (-34.625567, -58.416636)
+urquiza = (-34.624676, -58.409695)
+jujuy = (-34.623856, -58.402786)
+pichincha = (-34.623135, -58.397037)
+entrerios = (-34.622492, -58.391411)
+sanjose = (-34.622344, -58.385404)
+independencia_e = (-34.618244, -58.381583)
+belgrano_e = (-34.613005, -58.377820)
+bolivar = (-34.609576, -58.374026)
+
+lineaE = [virreyes,varela,mmilagrosa,emitre,jmmoreno,laplata,boedo,urquiza,jujuy,pichincha,entrerios,sanjose,independencia_e,\
+	belgrano_e,bolivar]
+
+
+#lineaH
+hospitales = (-34.641814, -58.412603)
+patricios = (-34.637539, -58.406008)
+caseros = (-34.635301, -58.399187 )
+inclan = (-34.629410, -58.400951 )
+humbertoI = (-34.622891, -58.402477)
+venezuela = (-34.615573, -58.404747)
+once = (-34.608373, -58.407008)
+corrientes = (-34.603766, -58.405368)
+cordoba = (-34.598681, -58.403789)
+santaFe = (-34.588115, -58.398262)
+
+lineaH = [hospitales,patricios,caseros,inclan,humbertoI,venezuela,once,corrientes,cordoba,santaFe]
+
+subtes = lineaA+lineaB+lineaC+lineaD+lineaE+lineaH
+
+#trenes
+belgranoR = (-34.567862, -58.462252) #Tren
+belgranoC = (-34.558538, -58.449600) #Linea mitre
+coghlan = (-34.565735, -58.475022) #Tren
+luisMariaSav = (-34.554436, -58.486803)
+drago = (-34.568488, -58.478846)
+devoto_a = (-34.595549, -58.510501)
+devoto_b = (-34.602456, -58.512831)
+smartin = (-34.601577, -58.494110)
+beiro = (-34.592979, -58.494106)
+elLibertador = (-34.594416, -58.502839)
+colegiales = (-34.573161, -58.448243)
+ciudadUniv = (-34.543366, -58.447282)
+estacionBuenosAires = (-34.645397, -58.394048)
+Yrigoyen = (-34.653450, -58.378623)
+avellaneda = (-34.661969, -58.376612)
+
+trenes =[belgranoR,belgranoC,coghlan,luisMariaSav,drago,devoto_a,devoto_b,smartin,beiro,elLibertador,colegiales,ciudadUniv,estacionBuenosAires,Yrigoyen,avellaneda]
+
+
+medios_transporte = trenes+subtes
